4460873ac3e700129b0e26749eeb358e9b1ba0d3
chura <chura24@yahoo.co.jp>
2017-07-09 06:18:42 +0000

    last_day

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,25 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.where(pref: params[:pref])
+  end
+
+  def show
+    @post = Post.where("pref = ?", params[:pref])
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
  root to: 'toppages#index'
  
+ get 'posts/:pref', to: 'posts#index', constraints: { pref: /[一-龠々]+/ }
+ get 'posts/:id', to: 'posts#show', constraints: { id: /d+/ }
+
  resources :posts
 end
